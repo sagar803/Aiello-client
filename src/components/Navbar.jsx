ecb8b5475540da1a99db48aab96c6136c0522e42
sagar chawla <sagarchawla83@gmail.com>
2023-04-30 14:45:38 +0530

    nav bar responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import './Navbar.css'
+
 export const Navbar = (props) => {
+  const [isMobile, setIsMobile] = useState(false);
   const homePage = () => {
     props.setPage('home')
   }
@@ -11,10 +14,31 @@ export const Navbar = (props) => {
     props.setPage('image')
   }
 
+  const menuItem = () => {
+    console.log("hir")
+    const navItem = document.querySelector(".nav-items");
+    navItem.classList.toggle("active");
+  }
+  /*
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 900);
+    };
+    handleResize(); // call the function once to set the initial state
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  */
+
   return (
     <nav>
         <p onClick={homePage} className='logo'>AIELLO</p>
-        <ul>
+        <div onClick={menuItem} className='hamburger'>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <ul className='nav-items'>
             <li onClick={homePage}>Home</li>
             <li onClick={ChatPage}>Chat assistance</li>
             <li onClick={ImagePage}>Generate images</li>
