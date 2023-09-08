5242b97363d18edf9c689ef43f73784f94fc7cc2
Sagar Chawla <sagarchawla83@gmail.com>
2023-09-08 12:23:09 +0530

    ui updates

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -13,17 +13,19 @@ export const Home = (Props) => {
         window.scrollTo(0, 0);
     }
     return (
-        <main>
-            <div className='logo-and-des'>
-                <img  className='main-logo' src={logo} alt="" />
-                <div className='main-des'>
-                    <p className='aiello-bullet'>
-                        Chat Assistance With Image Generation
+        <body>
+            <main className='landing-page-main'>
+                <div className='overlay'>
+                    <div className='logo-and-des'>
+                        <img  className='main-logo' src={logo} alt="logo" />
+                        <p className='des'>AIello is a versatile app that leverages AI technology to generate high-quality images and provide chat assistance for a wide range of tasks and topics, making it an essential tool for many industries.</p>
+                    </div>
+                    <p className='main-heading'>
+                        Introducing <em>Aiello</em>, All-in-One Creative Hub
                     </p>
-                <p className='main-sub-des'>AIello is a versatile app that leverages AI technology to generate high-quality images and provide chat assistance for a wide range of tasks and topics, making it an essential tool for many industries.</p>
                 </div>
-            </div>
-
+            </main>
+            
             <div className="card-container">
                 <div className='card query-card'>
                     <p className='card-heading'>Chat Assistance</p>
@@ -56,12 +58,6 @@ export const Home = (Props) => {
                     <button className='disabled'>Soon...</button>
                 </div>
             </div>
-            {/*
-            <div className='cta'>
-                <Query/>
-                <Image/>
-            </div>
-            */}
-        </main>
+        </body>
     )
 }
