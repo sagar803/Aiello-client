7a16d358affb92c5876084aa2c5a00e9c3bd803c
Sagar Chawla <sagarchawla83@gmail.com>
2023-09-10 18:25:50 +0530

    bug

--- src/scenes/Home/Home.jsx
+++ src/scenes/Home/Home.jsx
@@ -1,8 +1,7 @@
 import logo from '../../asset/logo.png'
 import './Home.css'
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion"
-
+import { motion } from 'framer-motion/dist/framer-motion'
 
 export const Home = () => {
     const navigate = useNavigate();
